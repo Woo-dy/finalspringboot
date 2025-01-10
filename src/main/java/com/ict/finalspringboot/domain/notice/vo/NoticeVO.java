31fed0090af8f8c28d58cbfa7a6226201fecb867
KIM WOO HYUN <kim03189@naver.com>
2025-01-11 00:20:14 +0900

    SpringBoot 250111 12:20

--- src/main/java/com/ict/finalspringboot/domain/notice/vo/NoticeVO.java
+++ src/main/java/com/ict/finalspringboot/domain/notice/vo/NoticeVO.java
@@ -1,5 +1,7 @@
 package com.ict.finalspringboot.domain.notice.vo;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NoticeVO {
-   private String notice_idx, admin_idx, notice_titlem, notice_content, notice_reg_date, notice_file, notice_file_name, notice_delete, notice_out_date;
+   private String notice_idx = "";
+   private String admin_idx = "";
+   private String notice_title = "";
+   private String notice_content = null;
+   private LocalDate notice_reg_date = LocalDate.of(2000, 1, 1);
+   private String notice_file = null;
+   private String notice_file_name = null;
+   private int notice_delete = 0;
+   private LocalDate notice_out_date = null;
 }
